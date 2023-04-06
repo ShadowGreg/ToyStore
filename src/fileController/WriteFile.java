9b8198f6a2c03558072da70eb580c3b660426df5
ShadowGreg <shadow-greg@yandex.ru>
2023-04-06 21:07:48 +0300

    пробую с json

--- src/fileController/WriteFile.java
+++ src/fileController/WriteFile.java
@@ -1,7 +1,11 @@
 package fileController;
 
-import java.io.FileWriter;
-import java.io.IOException;
+//import java.io.FileWriter;
+//import java.io.IOException;
+
+import org.json.simple.JSONObject;
+
+import java.util.List;
 
 public class WriteFile {
     private String path = "";
@@ -10,14 +14,25 @@ public class WriteFile {
         this.path = path;
     }
 
-    public void WriteText(String text) {
-        try (FileWriter writer = new FileWriter(this.path, true)) {
-            writer.write(text);
-            writer.append('\n');
+    public void WriteFile(List<String> toys){
+        try{
+            JSONObject sampleObject = new JSONObject();
 
-            writer.flush();
-        } catch (IOException ex) {
+
+        } catch (Exception ex){
             System.out.println(ex.getMessage());
         }
+
     }
+
+//    public void WriteText(String text) {
+//        try (FileWriter writer = new FileWriter(this.path, true)) {
+//            writer.write(text);
+//            writer.append('\n');
+//
+//            writer.flush();
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//    }
 }
